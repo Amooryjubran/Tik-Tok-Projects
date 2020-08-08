dc11b7e9b72555e631555eedfe1fe90caf5e6839
omar gubran <omarsalehjubran@gmail.com>
2020-08-08 17:29:08 -0400

    CI=true npm run build

---
+++ build/precache-manifest.606798276ab03fd25794d77917c2a2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669a3da6b34f961961946f06637ad1b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a74fbfc8efb9ff6fa150",
+    "url": "/static/css/main.94086979.chunk.css"
+  },
+  {
+    "revision": "509bcf50e3e4d63b22a8",
+    "url": "/static/js/2.57c00a7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.57c00a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74fbfc8efb9ff6fa150",
+    "url": "/static/js/main.dc8d38bc.chunk.js"
+  },
+  {
+    "revision": "6cefe96feca99028c5a6",
+    "url": "/static/js/runtime-main.9a4dce16.js"
+  }
+]);
