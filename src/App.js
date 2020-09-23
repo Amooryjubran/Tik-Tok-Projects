0a223c202a9cedc21bfa99b20f941e2632ba6594
amooryjubran <omarsalehjubran@gmail.com>
2020-09-23 14:49:35 -0400

    Video fixes ( wasn't showing )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import Video from "./Video";
+import Video__Kadeemh from "./vids/TikTok__1.mp4";
+import Video__Leenh from "./vids/TikTok__2.mp4";
+import Video__Jasonderulo from "./vids/TikTok__3.mp4";
 
 function App() {
   return (
@@ -8,7 +11,7 @@ function App() {
       <center>
         <div className="app__videos">
           <Video
-            url="http://download1654.mediafire.com/j5cq3jwea8pg/a18d1edgelq90mo/Kadeem+Hemmings+%E2%9A%A1%EF%B8%8F%E2%9C%A8+%28%40kadeemh%29+TikTok+-+Watch+Kadeem+Hemmings+%E2%9A%A1%EF%B8%8F%E2%9C%A8%5C%27s+Newest+TikTok+Videos.mp4"
+            url={Video__Kadeemh}
             channel="Kadeemh"
             description="Basement chronicles 4: shawarma medium spice please"
             song=" بام بام - محمد رمضان"
@@ -18,7 +21,7 @@ function App() {
           />
 
           <Video
-            url="http://download1642.mediafire.com/of6egljbwi9g/1gnwnkjetsmflqg/%D9%88%D8%A7%D9%84%D9%84%D9%87+%D8%AC%D8%AF%D9%88+%D8%B7%D9%84%D8%B9+%D9%85%D8%B9%D9%88+%D8%AD%D9%82+%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82+%23tiktokarab.mp4"
+            url={Video__Leenh}
             channel="Leenhabbal"
             description=" مين متلي ما بيشعر بالوقت عالtiktok? #tiktokarab #arab #middleeastern"
             song="Come and Get Your Love"
@@ -27,7 +30,7 @@ function App() {
             shares={12121}
           />
           <Video
-            url="http://download1083.mediafire.com/zq6sbop82x5g/5bd5aw8s1ez6cz9/Jason+Derulo+%28%40jasonderulo%29+on+TikTok+-+Nobody+gets+between+me+and+my+%40littlecaesarspizza+Stuffed+Crazy+Bread+%23GoCrazy+%23littlecaesars+%23ad+%23whoisUzo.mp4"
+            url={Video__Jasonderulo}
             channel="Jasonderulo"
             description=" Nobody gets between me and my @littlecaesarspizza Stuffed Crazy Bread"
             song="Go Crazy - Little Caesars"
